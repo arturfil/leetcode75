44930624d8e407149d4b39939886779d8e153d8d
arturfil <arturfil@hotmail.com>
2022-09-09 03:39:06 -0400

    added refactoring

--- Arrays/3sum.ts
+++ Arrays/3sum.ts
@@ -3,7 +3,8 @@ export function threeSum(nums: number[]): number[][] {
     nums.sort((a,b) => a - b);
     for(let i = 0; i < nums.length; i++) {
         let left = i+1, right = nums.length-1;
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // if repeated chars
+        // avoid repetitions in outer loop
+        if (i > 0 && nums[i] === nums[i - 1]) continue; 
         while (left < right) {
             if ( nums[i] + nums[left] + nums[right] < 0) {
                 left++;
@@ -12,14 +13,10 @@ export function threeSum(nums: number[]): number[][] {
             } else {
                 solution.push([nums[i], nums[left], nums[right]]);
                 left++;
+                // avoid repetitions in second loop
                 while (nums[left] === nums[left - 1] && left < right) left++;
             }
         }
     }
     return solution;
 }
-
-/*
-    let t = [-1,0,1,2,-1,-4]
-    console.log(threeSum(t));
-*/
