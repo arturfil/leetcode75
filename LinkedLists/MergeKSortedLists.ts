737c414e1f223d6a8ffaa689fc4dbdf8aeb15a9c
arturfil <arturfil@hotmail.com>
2022-09-13 19:18:15 -0400

    refactored the mergeksortedlists

--- LinkedLists/MergeKSortedLists.ts
+++ LinkedLists/MergeKSortedLists.ts
@@ -4,27 +4,20 @@ export function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
     let root = lists[0];
 
     for (let i = 1; i < lists.length; i++) {
-        root = mergeTwoLists(root, lists[i]);
+        root = mergeTwoSortedLists(root, lists[i]);
     }
     return root || null
 }
 
-export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    let tempNode = new ListNode(0);
-    let current = tempNode;
+export function mergeTwoSortedLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    if (list1 === null) return list2;
+    if (list2 === null) return list1;
 
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
-        }
-        current = current.next;
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoSortedLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoSortedLists(list1, list2.next);
+        return list2;
     }
-    if (list1) current.next = list1;
-    if (list2) current.next = list2;
-
-    return tempNode.next;
 }
--- LinkedLists/MergeTwoSortedLists.ts
+++ LinkedLists/MergeTwoSortedLists.ts
@@ -4,11 +4,11 @@ export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): L
     if (list1 === null) return list2;
     if (list2 === null) return list1;
     
-    if (list1.val >= list2.val) {
-        list1 = list1
-        return mergeTwoLists(list1.next, list2);
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
     } else {
-        list1 = list2;
-        return mergeTwoLists(list1, list2.next);
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
 }
