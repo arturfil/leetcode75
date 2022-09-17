3f4f04fe7a6589f6d56cc0bc27e6e20bf8527fa2
arturfil <arturfil@hotmail.com>
2022-09-17 00:50:25 -0400

    refactored course schedule problem

--- Graphs/CourseSchedule.ts
+++ Graphs/CourseSchedule.ts
@@ -2,10 +2,11 @@ export function canFinish(numCourses: number, prerequisites: number[][]): boolea
     const visited = new Set<number>();
     const current = new Set<number>();
     const preReqs = new Array<number[]>(numCourses).fill([]).map(_ => new Array().fill([]));
-
+    // you are pushing the courses IN the index of the prereq.
     for (let [c,p] of prerequisites) {
-        preReqs[p].push(c);
+        preReqs[c].push(p);
     }
+    // you will run the loop for each coure num.
     for (let i = 0; i < numCourses; i++) {
         if (!dfs(i, current, visited, preReqs)) return false;
     }
@@ -13,13 +14,16 @@ export function canFinish(numCourses: number, prerequisites: number[][]): boolea
 }
 
 function dfs(val: number,  current: Set<number>, visited: Set<number>, preReqs:number[][]) {
-    if (visited.has(val)) return true;
-    if (current.has(val)) return false;
-    current.add(val);
+    if (visited.has(val)) return true; // if visited is there return true, means the prereq checks
+    if (current.has(val)) return false; // if it's in current means at some point the dfs is false
+    current.add(val); // adding val to current
     for (let crs of preReqs[val]) {
+        // return false from a nested dfs call
         if (!dfs(crs, current, visited, preReqs)) return false;
     }
+    // delete from current
     current.delete(val);
+    // add to visited
     visited.add(val);
     return true;
 }
